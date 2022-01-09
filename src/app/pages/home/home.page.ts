d7ecd3090b553962dab0ac9f9e75e643b8757356
acanas <acanas@intelligencepartner.com>
2022-01-09 14:40:35 +0100

    feat: showed platform on home page

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DeviceService } from 'src/app/services/device.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  platform: string;
+  platformColor: string;
+
+  constructor(
+    private device: DeviceService
+  ) { }
+
+  ngOnInit(): void {
+    this.getPlatform();
+  }
+
+  async getPlatform() {
+    const platform = await this.device.getPlatform();
+    console.log(platform);
+
+    const PLATFORM_TITLE = {
+      android: 'Android',
+      ios: 'iOS',
+      web: 'Web'
+    };
+    const PLATFORM_COLOR = {
+      android: 'success',
+      ios: 'light',
+      web: 'primary'
+    };
+    this.platform = PLATFORM_TITLE[platform];
+    this.platformColor = PLATFORM_COLOR[platform];
+  }
 
 }
