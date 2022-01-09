293fbb499cecd22d502ca5cd541c8cebb0e9c09a
acanas <acanas@intelligencepartner.com>
2022-01-09 20:45:22 +0100

    test: movie detail mock

---
+++ spec/response/movie-detail.ts
@@ -0,0 +1,14 @@
+import { MovieDetail } from 'src/app/models/movie-detail/movie-detail.model';
+
+export const MOVIE_DETAIL = {
+    title: 'The Shawshank Redemption',
+    year: '1994',
+    rated: 'R',
+    released: '14 Oct 1994',
+    runtime: '142 min',
+    genre: 'Crime, Drama',
+    director: 'Frank Darabont',
+    writer: 'Stephen King (short story "Rita Hayworth and Shawshank Redemption")',
+    actors: 'Tim Robbins, Morgan Freeman, Bob Gunton, William Sadler',
+    plot: 'Two imprisoned'
+};
